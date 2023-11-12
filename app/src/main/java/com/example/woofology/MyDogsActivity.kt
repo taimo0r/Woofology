7f497a7333bcf5cc00689fc31eed8e47fe9c6571
Taimoor Mujahid <taimoormujahid83@gmail.com>
2023-11-12 12:34:10 -0500

    Creates MyDogActivity Layout

---
+++ app/src/main/java/com/example/woofology/MyDogsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.woofology
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyDogsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_dogs)
+    }
+}
