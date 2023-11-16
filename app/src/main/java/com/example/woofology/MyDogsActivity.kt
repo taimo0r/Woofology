66e574d9544c2caa996edf8ecca2ce1009611553
Taimoor Mujahid <taimoormujahid83@gmail.com>
2023-11-16 15:20:06 -0500

    [ #2 ] Implements Adding Picture from Camera/Gallery
    Closes #2

--- app/src/main/java/com/example/woofology/MyDogsActivity.kt
+++ app/src/main/java/com/example/woofology/MyDogsActivity.kt
@@ -2,18 +2,29 @@ package com.example.woofology
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.Dialog
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.ItemTouchHelper
@@ -23,12 +34,20 @@ import com.example.woofology.Database.DBManager
 import com.example.woofology.ModelClasses.Dog
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
 import java.util.LinkedList
 
 class MyDogsActivity : AppCompatActivity(), RecyclerItemTouchHelper.RecyclerItemTouchHelperListener{
 
-
+    private val GALLERY_REQUEST = 1
+    private val CAMERA_REQUEST = 2
     private lateinit var addDogFab: FloatingActionButton
+    private var newDogPhotoPath: String? = null
     private lateinit var addDogText: TextView
     private lateinit var recyclerView: RecyclerView
     private lateinit var dogsRVList: LinkedList<Dog>
@@ -54,7 +73,7 @@ class MyDogsActivity : AppCompatActivity(), RecyclerItemTouchHelper.RecyclerItem
                 } else if (event.action == MotionEvent.ACTION_UP) {
                     addDogFab.backgroundTintList =
                         ContextCompat.getColorStateList(this@MyDogsActivity, R.color.red)
-
+                    showPopUpMediaOptions()
                 }
                 return true
             }
@@ -77,7 +96,8 @@ class MyDogsActivity : AppCompatActivity(), RecyclerItemTouchHelper.RecyclerItem
             ActivityResultContracts.StartActivityForResult()
         ) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
-             // Todo: Make DogAnalysisActivity
+
+                //Todo: MakeDogAnalysisActivity
             }
         }
 
@@ -85,7 +105,6 @@ class MyDogsActivity : AppCompatActivity(), RecyclerItemTouchHelper.RecyclerItem
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.find_dog_activity -> return@setOnNavigationItemSelectedListener true
-
                 R.id.list_activity -> {
 
                 //Todo: Make AllBreedsActivity
@@ -112,14 +131,156 @@ class MyDogsActivity : AppCompatActivity(), RecyclerItemTouchHelper.RecyclerItem
 
     }
 
+    private fun showPopUpMediaOptions() {
+        val openDialog = Dialog(this@MyDogsActivity)
+        openDialog.setContentView(R.layout.select_camera_or_gallery_dialog)
+        val camera = openDialog.findViewById<TextView>(R.id.cameraBtn)
+        val gallery = openDialog.findViewById<TextView>(R.id.galleryBtn)
 
+        gallery.setOnClickListener {
+            loadImagefromGallery()
+            openDialog.dismiss()
+        }
+
+        camera.setOnClickListener {
+            callCameraPermissionAndValidation()
+            openDialog.dismiss()
+        }
+
+        openDialog.show()
+    }
+
+    private fun loadImagefromGallery() {
+        val galleryIntent = Intent(
+            Intent.ACTION_PICK,
+            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
+        startActivityForResult(galleryIntent, GALLERY_REQUEST)
+    }
+
+    private fun takePicture() {
+        val takePictureIntent = Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
+
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            var photoFile: File? = null
+            try {
+                photoFile = createImageFile()
+            } catch (ex: IOException) {
+                Log.e("Fail create image", ex.toString())
+            }
+            if (photoFile != null) {
+                val photoURI = FileProvider.getUriForFile(
+                    this,
+                    "com.example.woofology.fileprovider",
+                    photoFile
+                )
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                getPicture.launch(takePictureIntent)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        try {
+            if (data == null) {
+                return
+            }
+            if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK && null != data) {
+                val selectedImage: Uri = data.data!!
+                newDogPhotoPath = copyFileFromGallery(selectedImage)
+            }
+            if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK && null != data) {
+                // TODO Remove this comments
+            }
+
+            //Todo: Make DogAnalysisActivity
+
+        } catch (e: Exception) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+    private fun callCameraPermissionAndValidation() {
+        if (checkCallingOrSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            takePicture()
+        } else {
+            if (shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA)) {
+                val messageCamera = "Camera permission is needed"
+                Toast.makeText(this@MyDogsActivity, messageCamera, Toast.LENGTH_SHORT).show()
+            }
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA), CAMERA_REQUEST)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == CAMERA_REQUEST) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                takePicture()
+            } else {
+                val messagePermissions = "Permission was not granted"
+                Toast.makeText(this@MyDogsActivity, messagePermissions, Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+
+    private fun createImageFile(): File {
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageFileName = "JPEG_$timeStamp"
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val image = File.createTempFile(
+            imageFileName,  /* prefix */
+            ".jpg",         /* suffix */
+            storageDir      /* directory */
+        )
+        newDogPhotoPath = image.absolutePath
+        return image
+    }
+
+    private fun copyFileFromGallery(uriImageGallery: Uri): String {
+        val helperImgView = ImageView(this)
+        helperImgView.setImageURI(uriImageGallery)
+        val path = uriImageGallery.path
+        val bitmap = (helperImgView.drawable as BitmapDrawable).bitmap
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageFileName = "JPEG_$timeStamp"
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val image = File.createTempFile(
+            imageFileName,  /* prefix */
+            ".jpg",         /* suffix */
+            storageDir      /* directory */
+        )
+        val bos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100 /*ignored for PNG*/, bos)
+        val bitmapdata = bos.toByteArray()
+        val fos = FileOutputStream(image)
+        fos.write(bitmapdata)
+        fos.flush()
+        fos.close()
+        return image.absolutePath
+    }
 
     override fun onResume() {
         super.onResume()
         dogsRVLoadData()
     }
 
-
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder?, direction: Int, position: Int) {
+        if (viewHolder is DogRVListAdapter.MyViewHolder) {
+            val deletedDog = dogsRVList[viewHolder.adapterPosition]
+            val deletedIndex = viewHolder.adapterPosition
+            deleteDog(deletedDog, deletedIndex)
+            mAdapter.removeItem(viewHolder.adapterPosition)
+        }
+    }
 
 
     private fun dogsRVLoadData() {
@@ -132,7 +293,15 @@ class MyDogsActivity : AppCompatActivity(), RecyclerItemTouchHelper.RecyclerItem
         removeAddDogsMessage()
     }
 
-
+    private fun deleteDog(deletedDog: Dog, position: Int) {
+        val dbManager = DBManager(this, this)
+        dbManager.open()
+        dbManager.deleteDog(deletedDog.id)
+        dbManager.close()
+        val filePath = deletedDog.uriImage
+        val file = File(filePath)
+        file.delete()
+    }
 
      fun removeAddDogsMessage() {
         if (dogsRVList.size == 0) {
